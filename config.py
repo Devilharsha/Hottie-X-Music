996eee0905980b090da2662e803565c9f377c546
Kishore <89995228+AASFCYBERKING@users.noreply.github.com>
2022-02-23 13:51:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ load_dotenv()
 
 # VARS
 
+UPSTREAM_REPO = "https://github.com/Thiruselvan999/Hottie-X-Music"
 get_queue = {}
 BOT_TOKEN = getenv("BOT_TOKEN")
 API_ID = int(getenv("API_ID", ""))
@@ -20,9 +21,6 @@ MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
-UPSTREAM_REPO = getenv(
-    "UPSTREAM_REPO", "https://github.com/NotReallyShikhar/YukkiMusicBot"
-)
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
 if str(getenv("SUPPORT_CHANNEL")).strip() == "":
